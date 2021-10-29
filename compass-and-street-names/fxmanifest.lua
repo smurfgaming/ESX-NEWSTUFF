f9222580c40c5dcd51c6e2866e0efa6cda1207d7
smurfgaming <80631933+smurfgaming@users.noreply.github.com>
2021-10-29 15:36:29 +0100

    Add files via upload

---
+++ compass-and-street-names/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+description "MsQuerade's Compass and Streetname HUD"
+version "1.0"
+
+client_scripts {
+	"essentials.lua",
+	"compass.lua",
+	"streetname.lua"
+}
